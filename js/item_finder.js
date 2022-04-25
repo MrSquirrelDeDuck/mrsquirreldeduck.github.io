b0bdf2d67e04ddce9dbf77f7cfea8b46203d7a47
MrSquirrelDeDuck <72826048+MrSquirrelDeDuck@users.noreply.github.com>
2022-04-25 12:32:27 -0400

    Update item_finder.js

--- js/item_finder.js
+++ js/item_finder.js
@@ -16,11 +16,8 @@ function findItem() {
     }
     else {
 	    try {
-	    	/* let text_data = loadFile("resources/item_finder/items.txt");
-	    	document.getElementById("chestid").innerHTML = text_data; */
-		var fs = require('fs');
-		var text_data = fs.readFileSync("resources/item_finder/items.txt", 'utf-8');
-		var textByLine = text_data.split('\n')
+	    	let text_data = loadFile("resources/item_finder/items.txt");
+	    	document.getElementById("chestid").innerHTML = text_data;
             	console.log(text_data);
             	console.log(text_data.responseText);
 	    }
