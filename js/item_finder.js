223ff916e019564510ff71c2602164d2dce8a721
MrSquirrelDeDuck <72826048+MrSquirrelDeDuck@users.noreply.github.com>
2022-04-26 11:09:45 -0400

    Update item_finder.js

--- js/item_finder.js
+++ js/item_finder.js
@@ -13,7 +13,60 @@ function checkElement(elementText) {
   return elementText == document.getElementById("item_input").value;
 }
 
+function tickChest(chestid) {
+    chest_list = document.getElementsByClassName(chestid);
+    for (var i = 0; i < chest_list.length; i++) {
+	if (chest_list[i].classList.contains("red")){
+            chest_list[i].src = "resources/item_finder/chests/chest_red_ticked.png";
+        }
+	if (chest_list[i].classList.contains("orange")){
+            chest_list[i].src = "resources/item_finder/chests/chest_orange_ticked.png";
+        }
+	if (chest_list[i].classList.contains("yellow")){
+            chest_list[i].src = "resources/item_finder/chests/chest_yellow_ticked.png";
+        }
+	if (chest_list[i].classList.contains("green")){
+            chest_list[i].src = "resources/item_finder/chests/chest_green_ticked.png";
+        }
+	if (chest_list[i].classList.contains("mblue")){
+            chest_list[i].src = "resources/item_finder/chests/chest_medium_blue_ticked.png";
+        }
+	if (chest_list[i].classList.contains("lblue")){
+            chest_list[i].src = "resources/item_finder/chests/chest_light_blue_ticked.png";
+        }
+	if (chest_list[i].classList.contains("dblue")){
+            chest_list[i].src = "resources/item_finder/chests/chest_dark_blue_ticked.png";
+        }
+	if (chest_list[i].classList.contains("purple")){
+            chest_list[i].src = "resources/item_finder/chests/chest_purple_ticked.png";
+        }
+	if (chest_list[i].classList.contains("pink")){
+            chest_list[i].src = "resources/item_finder/chests/chest_pink_ticked.png";
+        }
+    }
+}
+
 function findItem() {
+    var chest_red = "resources/item_finder/chests/chest_red.png";
+    var chest_orange = "resources/item_finder/chests/chest_orange.png";
+    var chest_yellow = "resources/item_finder/chests/chest_yellow.png";
+    var chest_green = "resources/item_finder/chests/chest_green.png";
+    var chest_mblue = "resources/item_finder/chests/chest_medium_blue.png";
+    var chest_lblue = "resources/item_finder/chests/chest_light_blue.png";
+    var chest_dblue = "resources/item_finder/chests/chest_dark_blue.png";
+    var chest_purple = "resources/item_finder/chests/chest_purple.png";
+    var chest_pink = "resources/item_finder/chests/chest_pink.png";
+
+    var chest_red_ticked = "resources/item_finder/chests/chest_red_ticked.png";
+    var chest_orange_ticked = "resources/item_finder/chests/chest_orange_ticked.png";
+    var chest_yellow_ticked = "resources/item_finder/chests/chest_yellow_ticked.png";
+    var chest_green_ticked = "resources/item_finder/chests/chest_green_ticked.png";
+    var chest_mblue_ticked = "resources/item_finder/chests/chest_medium_blue_ticked.png";
+    var chest_lblue_ticked = "resources/item_finder/chests/chest_light_blue_ticked.png";
+    var chest_dblue_ticked = "resources/item_finder/chests/chest_dark_blue_ticked.png";
+    var chest_purple_ticked = "resources/item_finder/chests/chest_purple_ticked.png";
+    var chest_pink_ticked = "resources/item_finder/chests/chest_pink_ticked.png";
+	
     var textInput = document.getElementById("item_input").value;
     if (textInput == ""){
         document.getElementById("chestid").innerHTML = "Please put something in the text box.";
@@ -28,6 +81,7 @@ function findItem() {
 		    chest_id = id_list[parseInt(item_list.findIndex(checkElement),10)];
 		    if (chest_id != -1) {
 		    	document.getElementById("chestid").innerHTML = "Chest id: " + chest_id;
+			tickChest(chest_id);
 		    } else {
 			document.getElementById("chestid").innerHTML = "Item is not obtainable in-game."
 		    }
