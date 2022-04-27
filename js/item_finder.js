a2625d4cd1e55fb472993d9ef06bfbfe62caf3c1
MrSquirrelDeDuck <72826048+MrSquirrelDeDuck@users.noreply.github.com>
2022-04-27 19:39:04 -0400

    Update item_finder.js

--- js/item_finder.js
+++ js/item_finder.js
@@ -160,7 +160,16 @@ function changeBackground() {
 function checkCustom() {
 	document.getElementById("logo_image").src = "resources/item_finder/terraria_chest.png";
 	var userInput = document.getElementById("item_input").value;
-	if (userInput == "Bamboo Chest") {
-		document.getElementById("logo_image").src = "resources/item_finder/otherchests/chest_bamboo.png";
+	let text_data_4 = loadFile("resources/item_finder/otherchests/chest_inputs.txt");
+	const chest_inputs = text_data4.split("\n");
+	let text_data_5 = loadFile("resources/item_finder/otherchests/chest_urls.txt");
+	const chest_urls = text_data5.split("\n");
+	
+	if (chest_inputs.includes(userInput)) {
+		document.getElementById("logo_image").src = chest_urls[parseInt(chest_inputs.findIndex(checkElement),10)];
 	}
+	
+	/* if (userInput == "Bamboo Chest") {
+		document.getElementById("logo_image").src = "resources/item_finder/otherchests/chest_bamboo.png";
+	} */
 }
